60c67fb58fb597ebed91b56e54aa622ce74f42ca
ryuun1corn <yuda.arif.prasojo@gmail.com>
2023-08-24 22:33:21 +0700

    added 'qrcode' command

---
+++ commands/utilities/qrcode.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+const fs = require('node:fs');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('qrcode')
+		.setDescription('Turn a text into QR code.')
+        .addStringOption(option => 
+            option
+                .setName("text")
+                .setDescription("The text to convert to QR code")
+                .setRequired(true)),
+	async execute(interaction) {
+        const text=interaction.options.getString('text');
+        await interaction.reply({
+            content: `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(text)}`,
+        });
+	},
+};
+
