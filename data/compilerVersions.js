7fb7923a4afc2d36844f00084d86e0cac0917d60
ryuun1corn <yuda.arif.prasojo@gmail.com>
2023-08-26 16:03:08 +0700

    added key-value pairs as compiler data

---
+++ data/compilerVersions.js
@@ -0,0 +1,69 @@
+module.exports = {
+    "python": {
+        "Python 3.11": "python311",
+        "Python 3.10": "python310",
+        "Python 3.9": "python39",
+        "Python 3.8": "python38",
+        "Python 3.7": "python37",
+        "Python 3.6": "python36",
+        "Python 3.5": "python35"
+    },
+    "java": {
+        "jdk 11.0.2": "java1102",
+        "jdk 12.0.1": "java1201",
+        "jdk 13.0.2": "java1302",
+        "jdk 14.0.2": "java1402",
+        "jdk 15.0.2": "java1502",
+        "jdk 16.0.1": "java1601",
+        "jdk 17.0.0": "java1700",
+        "jdk 18.0.0": "java1800",
+        "jdk 19.0.2": "java1902",
+        "jdk 20.0.0": "java2000"
+    },
+    "javascript": {
+        "v8 (trunk)": "v8trunk",
+        "v8 11.3": "v8113"
+    },
+    "ruby": {
+        "Ruby 2.5.9": "ruby259",
+        "Ruby 2.6.8": "ruby268",
+        "Ruby 2.7.4": "ruby274",
+        "Ruby 3.0.2": "ruby302"
+    },
+    "csharp": {
+        ".NET (main)": "dotnettrunkcsharp",
+        ".NET 6.0.110": "dotnet6011csharp",
+        ".NET 6.0.113": "dotnet6014csharp",
+        ".NET 6.0.116": "dotnet6018csharp",
+        ".NET 7.0.100": "dotnet701csharp",
+        ".NET 7.0.102": "dotnet703csharp",
+        ".NET 7.0.105": "dotnet707csharp"
+    },
+    "c++": {
+        "x64 msvc v19.latest": "vcpp_v19_latest_x64",
+        "x86 msvc v19.latest": "vcpp_v19_latest_x86",
+        "x86-64 clang (trunk)": "clang_trunk",
+        "x86-64 gcc (trunk)": "gsnapshot"
+    },
+    "c": {
+        "ARM GCC trunk (linux)": "carmgtrunk",
+        "ARM64 GCC trunk": "carm64gtrunk",
+        "armv7-a clang (trunk)": "armv7-cclang-trunk",
+        "armv8-a clang (trunk)": "armv8-cclang-trunk",
+        "mips gcc 11.2.0": "cmips1120",
+        "mips64 gcc 11.2.0": "cmips112064",
+        "x86-64 clang (trunk)": "cclang_trunk",
+        "x86-64 gcc (trunk)": "cgsnapshot"
+    },
+    "rust": {
+        "BPF gcc (trunk)": "rustbpfgtrunk",
+        "mrustc (master)": "mrustc-master",
+        "rustc-cg-gcc (master)": "rustccggcc-master"
+    },
+    "go": {
+        "ARM gc (tip)": "arm_gltip",
+        "ARM64 gc (tip)": "arm64_gltip",
+        "x86 gc (tip)": "386_gltip",
+        "x86-64 gc (tip)": "gltip"
+    }
+}
